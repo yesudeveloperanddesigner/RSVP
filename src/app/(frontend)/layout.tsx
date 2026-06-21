b2894207e65df40c7111e6dea41b08d65e214de8
pat22292 <madlangawapat22@gmail.com>
2026-06-21 23:25:12 +0800

    test tiktok

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -23,29 +23,36 @@ const greatVibes = Great_Vibes({
   display: 'swap',
 })
 
-const siteUrl = process.env.PAYLOAD_PUBLIC_SERVER_URL || (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000')
+const siteUrl =
+  process.env.PAYLOAD_PUBLIC_SERVER_URL ||
+  (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000')
 
 export const metadata = {
-  description: 'You are cordially invited to celebrate the wedding of Cheley & Denz on June 21, 2026 at Coral Sands Beach Resort.',
+  description:
+    'You are cordially invited to celebrate the wedding of Cheley & Denz on June 21, 2026 at Coral Sands Beach Resort.',
   title: 'Cheley & Denz | Wedding Invitation',
   metadataBase: new URL(siteUrl),
   openGraph: {
     type: 'website',
     title: 'Cheley & Denz | Wedding Invitation',
-    description: 'You are cordially invited to celebrate the wedding of Cheley & Denz on June 21, 2026 at Coral Sands Beach Resort.',
+    description:
+      'You are cordially invited to celebrate the wedding of Cheley & Denz on June 21, 2026 at Coral Sands Beach Resort.',
     siteName: 'Cheley & Denz Wedding',
     url: '/',
-    images: [{
-      url: '/screenshot.png',
-      width: 1200,
-      height: 630,
-      alt: 'Cheley & Denz Wedding Invitation',
-    }],
+    images: [
+      {
+        url: '/screenshot.png',
+        width: 1200,
+        height: 630,
+        alt: 'Cheley & Denz Wedding Invitation',
+      },
+    ],
   },
   twitter: {
     card: 'summary_large_image',
     title: 'Cheley & Denz | Wedding Invitation',
-    description: 'You are cordially invited to celebrate the wedding of Cheley & Denz on June 21, 2026 at Coral Sands Beach Resort.',
+    description:
+      'You are cordially invited to celebrate the wedding of Cheley & Denz on June 21, 2026 at Coral Sands Beach Resort.',
     images: ['/screenshot.png'],
   },
 }
